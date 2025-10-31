612f2bfb2b5a82edbb2daf2be254de8c0681cdcf
Franco <bounszz@hotmail.com>
2025-10-30 23:41:40 -0500

    agregando about, cambiando clases css en cards

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
+import { Footer } from './footer/footer';
 import { Cards } from "./cards/cards";
 import { Hero } from "./hero/hero";
+import { About } from "./about/about";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Header, Hero, Cards],
+  imports: [RouterOutlet, Header, Hero, About, Cards, Footer],
   templateUrl: './app.html',
   styleUrls: ['./app.scss']
 })
