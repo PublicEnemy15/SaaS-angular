546878a6050887c176f08f0cdeee24afe64ef15f
Franco <bounszz@hotmail.com>
2025-11-02 18:05:01 -0500

    creando login y register con las rutas dinamicas (aun no validado el header dependiendo la ruta)

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,25 +1,37 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';  
-import { CommonModule } from '@angular/common';  
-import { RouterLink } from '@angular/router'; 
-import { UserActions } from '../user-actions/user-actions';  
-  
-@Component({  
-  selector: 'app-header',  
-  standalone: true,  
-  imports: [CommonModule, UserActions, RouterLink],
-  templateUrl: './header.html',  
-  styleUrls: ['./header.scss'],  
-  changeDetection: ChangeDetectionStrategy.OnPush 
-})  
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
+import { UserActions } from '../user-actions/user-actions';
 
-export class Header {  
-  readonly isMenuOpen = signal<boolean>(false);  
-  
-  toggleMenu(): void {  
-    this.isMenuOpen.update((v) => !v);  
-  }  
-  
-  closeMenu(): void {  
-    this.isMenuOpen.set(false);  
-  }  
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, UserActions, RouterLink],
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class Header implements OnInit {
+  readonly isMenuOpen = signal(false);
+  readonly isLoggedIn = signal(false);
+  url = signal('');
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.url.set(event.urlAfterRedirects.replace('/', ''));
+      });
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen.update(v => !v);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
 }
--- src/app/user-actions/user-actions.ts
+++ src/app/user-actions/user-actions.ts
@@ -1,12 +1,15 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';  
-import { RouterLink } from '@angular/router';  
-  
-@Component({  
-  selector: 'app-user-actions',  
-  standalone: true,  
-  imports: [RouterLink],  
-  templateUrl: './user-actions.html',  
-  styleUrls: ['./user-actions.scss'],  
-  changeDetection: ChangeDetectionStrategy.OnPush  
-})  
-export class UserActions {}
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-user-actions',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './user-actions.html',
+  styleUrls: ['./user-actions.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class UserActions {
+  @Input() url: string = '';
+  @Input() isLoggedIn = false;
+}
