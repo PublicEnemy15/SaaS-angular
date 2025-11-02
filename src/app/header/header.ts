4c39ce0bafe980400ca90e73ff2f3658925cb960
Franco <bounszz@hotmail.com>
2025-11-02 18:23:56 -0500

    cambiando routes.ts

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,25 +1,22 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';  
-import { CommonModule } from '@angular/common';  
-import { RouterLink } from '@angular/router'; 
-import { UserActions } from '../user-actions/user-actions';  
-  
-@Component({  
-  selector: 'app-header',  
-  standalone: true,  
-  imports: [CommonModule, UserActions, RouterLink],
-  templateUrl: './header.html',  
-  styleUrls: ['./header.scss'],  
-  changeDetection: ChangeDetectionStrategy.OnPush 
-})  
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserActions } from '../user-actions/user-actions';
 
-export class Header {  
-  readonly isMenuOpen = signal<boolean>(false);  
-  
-  toggleMenu(): void {  
-    this.isMenuOpen.update((v) => !v);  
-  }  
-  
-  closeMenu(): void {  
-    this.isMenuOpen.set(false);  
-  }  
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [CommonModule, UserActions],
+  templateUrl: './header.html',
+  styleUrls: ['./header.scss'],
+})
+export class Header {
+  isMenuOpen = signal(false);
+
+  toggleMenu(): void {
+    this.isMenuOpen.update((v) => !v);
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen.set(false);
+  }
 }
