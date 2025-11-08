3d3595ac428cbb33ceb6b7edb7b219e81da92ead
Yefferson <yeffersonfarfanmallqui30@gmail.com>
2025-11-08 00:20:27 -0500

    validacion de registro

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -10,18 +10,45 @@ import { AuthService } from '../services/auth.service';
   imports: [CommonModule, FormsModule],
   templateUrl: './register.html',
   styleUrls: ['./register.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Register {
-  
+
   name = '';
   email = '';
   password = '';
   message = '';
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService) { }
+
+  private validacionemail(email: string): boolean {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email)
+  }
 
   register() {
+
+    if (!this.name.trim() || !this.email.trim() || !this.password.trim()) {
+        this.message = 'Rellenar todos los campos'
+      console.warn('Formulario incompleto', {
+        name: this.name,
+        email: this.email,
+        password: this.password
+      });
+      return;
+    }
+
+    if (!this.validacionemail(this.email)) {
+      this.message = 'Correo inválido';
+      console.warn('Correo invalido:', this.email);
+      return;
+    }
+
+    if (this.password.length < 8) {
+      console.warn('La contraseña tiene que tener al menos 8 caracteres')
+      this.message = 'La contraseña tiene que tener al menos 8 valores';
+      return;
+    }
+
     const data = { name: this.name, mail: this.email, pwd: this.password };
     console.log('Datos enviados al backend:', data);
     this.auth.register(data).subscribe({
